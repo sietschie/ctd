10e8dade96c456697e117b0ba033720267ae2b57
sietschie <sietschie@gmail.com>
2010-07-22 16:45:36 +0200

    added one bullet firing tower

--- ctd.py
+++ ctd.py
@@ -1,4 +1,10 @@
-import curses, sys, traceback, time
+import curses, sys, traceback, time, math
+
+class tower:
+	x = 12
+	y = 8
+	last_shoot = 0
+	speed = 3
 
 # global variables
 class gb:
@@ -9,6 +15,8 @@ class gb:
 	current_time = 0
 	last_time = 0
 	minions = []
+	tower = tower()
+	bullets = []
 	max_x = 20
 	max_y = 20
 
@@ -20,9 +28,123 @@ class minion:
 	speed = 1
 	current_wp = 1
 
+class bullet:
+	x = 0
+	y = 0
+	dx = 0
+	dy = 0
+	speed = 1.5
+
+
+
 def draw_minions():
 	for m in gb.minions:
 		draw_at(int(m.x),int(m.y),'o', curses.COLOR_BLACK, curses.COLOR_YELLOW)
+
+def draw_bullets():
+	for b in gb.bullets:
+		if gb.the_map[int(b.x),int(b.y)] == 0:
+			draw_at(int(b.x),int(b.y),'*', curses.COLOR_BLACK, curses.COLOR_GREEN)
+		else:
+			draw_at(int(b.x),int(b.y),'*', curses.COLOR_BLACK, curses.COLOR_YELLOW)
+
+def animate_bullets():
+	delta = gb.current_time - gb.last_time
+	for b in gb.bullets:
+		b.x += delta * b.dx
+		b.y += delta * b.dy
+		if int(b.x) < 1:
+			gb.bullets.remove(b)
+		if int(b.x) > gb.max_x:
+			gb.bullets.remove(b)
+		if int(b.y) < 1:
+			gb.bullets.remove(b)
+		if int(b.y) > gb.max_y:
+			gb.bullets.remove(b)
+
+def collision_detection():
+	for b in gb.bullets:
+		for m in gb.minions:
+			if int(b.x) == int(m.x):
+				if int(b.y) == int(m.y):
+					gb.bullets.remove(b)
+					gb.minions.remove(m)
+
+def shoot(ct, cm):
+	t = vorhalt(ct,cm)
+	nb =  bullet()
+	nb.x = float(ct.x)
+	nb.y = float(ct.y)
+
+	diff_x = (cm.x + t * cm.dx) - nb.x
+	diff_y = (cm.y + t * cm.dy) - nb.y
+	
+	dist = math.sqrt( diff_x * diff_x + diff_y * diff_y )
+
+	nb.dx = nb.speed * float(diff_x) / float(dist)
+	nb.dy = nb.speed * float(diff_y) / float(dist)
+
+	gb.bullets.append(nb)
+
+def vorhalt(ct, cm):
+	a = cm.dx * cm.dx + cm.dy * cm.dy - bullet.speed * bullet.speed;
+	b = 2 * (cm.x - ct.x) * cm.dx + 2*(cm.y - ct.y) * cm.dy;
+	c = math.pow( cm.x - ct.x , 2) + math.pow( cm.y - ct.y, 2);
+
+	if a == 0: #vllt mit epsilon?
+		t = -c / b;
+		gb.scrn.addstr(20,20,str( t ))
+		gb.scrn.addstr(21,21,str( b ))
+		gb.scrn.addstr(22,22,str( c ))
+		gb.scrn.addstr(23,23,str( cm.dy ))
+		gb.scrn.addstr(24,24,str( ct.y ))
+		gb.scrn.addstr(25,25,str( cm.y ))
+		gb.scrn.addstr(26,26,str( cm.dx ))
+		gb.scrn.addstr(27,27,str( ct.x ))
+		gb.scrn.addstr(28,28,str( cm.x ))
+
+	else: # fuer speed ungleich 1, nicht getestet.
+		t1 = ( - b + math.sqrt( b * b - 4 * a * c))/(2*a);
+		t2 = ( - b - math.sqrt( b * b - 4 * a * c))/(2*a);
+
+		if t1 > 0:
+			if t2 > 0:
+				t = min(t1,t2)
+			else:
+				t = t1
+		else:
+			if t2 > 0:
+				t = t2
+			#else:
+				# irgendnen fehler?			
+	return t
+	
+	
+def find_target(ct):
+	min_dist = 100000
+	min_min = 0
+	for m in gb.minions:
+		diff_x = ct.x - m.x
+		diff_y = ct.y - m.y
+	
+		dist = math.sqrt( diff_x * diff_x + diff_y * diff_y )
+
+		if dist < min_dist:
+			min_dist = dist
+			min_min = m
+
+	if min_dist < 5:
+		return min_min
+	else:
+		return 0
+
+def animate_tower():
+	if gb.current_time - gb.tower.last_shoot > gb.tower.speed: 
+		m = find_target(gb.tower)
+
+		if m != 0:
+			shoot(gb.tower, m)	
+			gb.tower.last_shoot = gb.current_time
 
 def animate_minions():
 	delta = gb.current_time - gb.last_time
@@ -61,6 +183,8 @@ def animate_minions():
 			gb.minions.remove(m)
 		if int(m.y) > gb.max_y:
 			gb.minions.remove(m)
+def draw_tower():
+	draw_at(int(gb.tower.x),int(gb.tower.y),'#', curses.COLOR_BLACK, curses.COLOR_GREEN)
 
 def draw_map():
 	for x in range(1,21):
@@ -142,9 +266,14 @@ def main():
 				gb.minions.append(minion())
 		gb.last_time = gb.current_time
 		gb.current_time = time.time()
+		collision_detection()
 		animate_minions()
+		animate_tower()
+		animate_bullets()
 		draw_map()
 		draw_minions()
+		draw_tower()
+		draw_bullets()
 
 	# restore original settings
 	restorescreen()

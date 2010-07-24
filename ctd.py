1678b60736d9352e6f292f0062ee4b667e7bd292
sietschie <sietschie@gmail.com>
2010-07-24 10:51:27 +0200

    system module added

--- ctd.py
+++ ctd.py
@@ -1,10 +1,8 @@
-import curses, sys, traceback, time, logic
+import time, logic, system, traceback
 
 
 # global variables
 class gb:
-	scrn = None # will point to window object
-	mapping = {}
 	current_time = 0
 	last_time = 0
 	delta = current_time - last_time
@@ -12,88 +10,47 @@ class gb:
 
 def draw_minions():
 	for m in logic.minions:
-		draw_at(int(m.x),int(m.y),'o', curses.COLOR_BLACK, curses.COLOR_YELLOW)
+		system.draw_at(int(m.x),int(m.y),'o', system.COLOR_BLACK, system.COLOR_YELLOW)
 
 def draw_bullets():
 	for b in logic.bullets:
 		if logic.a_map.tiles[int(b.x),int(b.y)] == 0:
-			draw_at(int(b.x),int(b.y),'*', curses.COLOR_BLACK, curses.COLOR_GREEN)
+			system.draw_at(int(b.x),int(b.y),'*', system.COLOR_BLACK, system.COLOR_GREEN)
 		else:
-			draw_at(int(b.x),int(b.y),'*', curses.COLOR_BLACK, curses.COLOR_YELLOW)
+			system.draw_at(int(b.x),int(b.y),'*', system.COLOR_BLACK, system.COLOR_YELLOW)
 
 
 
 
 def draw_towers():
 	for t in logic.towers:
-		draw_at(int(t.x),int(t.y),'#', curses.COLOR_BLACK, curses.COLOR_GREEN)
+		system.draw_at(int(t.x),int(t.y),'#', system.COLOR_BLACK, system.COLOR_GREEN)
 
 def draw_map():
 	for x in range(1,21):
 		for y in range(1,21):
 			if logic.a_map.tiles[x,y] == 0:
-				draw_at(x,y,' ', curses.COLOR_GREEN, curses.COLOR_GREEN)
+				system.draw_at(x,y,' ', system.COLOR_GREEN, system.COLOR_GREEN)
 			else:
-				draw_at(x,y,' ', curses.COLOR_YELLOW, curses.COLOR_YELLOW)
+				system.draw_at(x,y,' ', system.COLOR_YELLOW, system.COLOR_YELLOW)
 
-def draw_at(x,y,chr,fg,bg):
-	gb.scrn.addch(x,y,chr,curses.color_pair(gb.mapping[fg,bg]))
 
-# this code is vital; without this code, your terminal would be unusable
-# after the program exits
-def restorescreen():
-	# restore "normal"--i.e. wait until hit Enter--keyboard mode
-	curses.nocbreak()
-	# restore keystroke echoing
-	curses.echo()
-	# required cleanup call
-	curses.endwin()
 
 def main():
-	# first we must create a window object; it will fill the whole screen
-	gb.scrn = curses.initscr()
-	# turn off keystroke echo
-	curses.noecho()
-	# keystrokes are honored immediately, rather than waiting for the
-	# user to hit Enter
-	curses.cbreak()
-
-	gb.scrn.keypad(1)
-
-	gb.scrn.nodelay(1)
-	# start color display (if it exists; could check with has_colors())
-	curses.start_color()
-
-	curses.mousemask(curses.BUTTON1_PRESSED)
-
-	# clear screen
-	gb.scrn.clear()
-
-
-	list_of_colors = [ curses.COLOR_BLACK, curses.COLOR_BLUE, curses.COLOR_CYAN, curses.COLOR_GREEN, curses.COLOR_MAGENTA, curses.COLOR_RED, curses.COLOR_WHITE, curses.COLOR_YELLOW  ]
-
-	i = 1;
-	for bg in list_of_colors:
-		for fg in list_of_colors:
-			curses.init_pair(i,fg,bg)
-			gb.mapping[fg,bg] = i
-			i+=1
+	system.init()
 
 	logic.init_map()
 
 	last_time = time.time()
 	current_time =  time.time()
 
-	# implement the actions done so far (just the clear())
-	gb.scrn.refresh()
-
 	while True:
 		# read character from keyboard
-		c = gb.scrn.getch()
+		c = system.getch()
 		# was returned as an integer (ASCII); make it a character
 		if c != -1:
-			if c == curses.KEY_MOUSE:
-				id,y,x,z,button = curses.getmouse()
+			if c == system.KEY_MOUSE:
+				id,y,x,z,button = system.getmouse()
 				if x >= 1 :
 					if x <= logic.a_map.max_x : 
 						if y >= 1 :
@@ -137,7 +94,7 @@ if __name__ =='__main__':
 	try:
 		main()
 	except:
-		restorescreen()
+		system.restorescreen()
 		# print error message re exception
 		traceback.print_exc()
 
---
+++ system.py
@@ -0,0 +1,62 @@
+import curses, sys
+
+scrn = None # will point to window object
+mapping = {}
+list_of_colors = [ curses.COLOR_BLACK, curses.COLOR_BLUE, curses.COLOR_CYAN, curses.COLOR_GREEN, curses.COLOR_MAGENTA, curses.COLOR_RED, curses.COLOR_WHITE, curses.COLOR_YELLOW  ]
+KEY_MOUSE = curses.KEY_MOUSE
+COLOR_BLACK = curses.COLOR_BLACK
+COLOR_GREEN = curses.COLOR_GREEN
+COLOR_YELLOW = curses.COLOR_YELLOW
+getch = None
+getmouse = curses.getmouse
+
+def draw_at(x,y,chr,fg,bg):
+	scrn.addch(x,y,chr,curses.color_pair(mapping[fg,bg]))
+
+def init():
+	# first we must create a window object; it will fill the whole screen
+	global scrn
+	scrn = curses.initscr()
+	# turn off keystroke echo
+	curses.noecho()
+	# keystrokes are honored immediately, rather than waiting for the
+	# user to hit Enter
+	curses.cbreak()
+
+	scrn.keypad(1)
+
+	scrn.nodelay(1)
+	# start color display (if it exists; could check with has_colors())
+	curses.start_color()
+
+	curses.mousemask(curses.BUTTON1_PRESSED)
+
+	global getch
+	getch = scrn.getch
+
+	# clear screen
+	scrn.clear()
+
+	i = 1;
+	for bg in list_of_colors:
+		for fg in list_of_colors:
+			curses.init_pair(i,fg,bg)
+			mapping[fg,bg] = i
+			i+=1
+
+	# implement the actions done so far (just the clear())
+	scrn.refresh()
+
+
+
+# this code is vital; without this code, your terminal would be unusable
+# after the program exits
+def restorescreen():
+	# restore "normal"--i.e. wait until hit Enter--keyboard mode
+	curses.nocbreak()
+	# restore keystroke echoing
+	curses.echo()
+	# required cleanup call
+	curses.endwin()
+
+

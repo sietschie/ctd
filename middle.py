cf5db8b443fbefe1cc6a20b2128b35d61e4bc5b9
sietschie <sietschie@gmail.com>
2010-09-11 19:14:53 +0200

    fixed: draw maps of other sizes than 20x20

--- middle.py
+++ middle.py
@@ -45,8 +45,8 @@ class Middle:
 
     def draw_map(self):
         """Draws map to screen."""
-        for x in range(1, 21):
-            for y in range(1, 21):
+        for x in range(1, self.logic.current_level.max_x + 1):
+            for y in range(1, self.logic.current_level.max_y + 1):
                 if self.logic.current_level.tiles[x, y] == 0:
                     self.system.draw_at(Vector(x, y), ' ', 
                         self.system.COLOR_GREEN, self.system.COLOR_GREEN)
@@ -156,7 +156,7 @@ class Middle:
         self.system = System(self.evm)
         self.logic = Logic(self.evm)
         
-        self.load_map('map.xml')
+        self.load_map('map3.xml')
 
     def run(self):
         """The main game loop"""

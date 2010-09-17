019851da567bf3821a735d4ac9a5dd4cb2e2c517
sietschie <sietschie@gmail.com>
2010-09-17 17:38:14 +0200

    renamed README

---
+++ README.mk
@@ -0,0 +1,11 @@
+Commandline Tower Defence
+=========================
+A simple Tower Defence game using ncurses.
+
+Playing the game
+----------------
+Press space to send next wave. Hit q to quit the game. 
+Click anywhere on the map to place a tower.
+
+If there are no more minions or you lost all your lives the game 
+quits.
